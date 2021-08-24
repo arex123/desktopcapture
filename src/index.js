4cab056356640d7369b61774ead33e8e9679088c
arex123 <ad47kumar@gmail.com>
2021-08-24 23:46:57 +0530

    electron packager

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.removeMenu(); // to remove
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
